a600b47a9013f4b7308655e819a8fab1ff79d59d
owozsh <accounts@owozsh.dev>
2022-05-17 20:45:24 -0300

    remove frontend from docs repository

--- app/frontend/src/components/widgets/TaskView.tsx
+++
@@ -1,7 +0,0 @@
-export default function TaskView({
-  children,
-}: {
-  children?: JSX.Element[] | JSX.Element;
-}) {
-  return <ul className="shrink h-full w-full">{children}</ul>;
-}
--- app/frontend/src/pages/index.tsx
+++
@@ -1,6 +0,0 @@
-import AppLayout from "../components/layouts/AppLayout";
-import SidebarButton from "../components/widgets/SidebarButton";
-
-export default function App() {
-  return <AppLayout />;
-}
